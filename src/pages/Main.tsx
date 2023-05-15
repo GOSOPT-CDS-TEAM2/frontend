3256f7a096538584abcf2ac3aeb1e52788d8c544
seobbang <seobbangway@naver.com>
2023-05-15 14:00:49 +0900

    feat/#2: 카테고리 nav 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
+import CategoryNav from '../components/Main/CategoryNav';
 import Header from '../components/Main/Header';
+import MenuSection from '../components/Main/MenuSection';
 
 const Main = () => {
   return (
     <St.MainContainer>
       <Header />
+      <CategoryNav />
+      <MenuSection />
     </St.MainContainer>
   );
 };
@@ -13,6 +17,10 @@ const Main = () => {
 export default Main;
 const St = {
   MainContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     width: 100vw;
   `,
 };
