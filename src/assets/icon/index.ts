853bb6be75e3adc9fb5abdd2d4d31ebdb9d70ec2
HeeJungPark <phj7214na@naver.com>
2023-05-15 12:06:17 +0900

    chore/#1: asset Backicon, BagIcon, SearchIcon 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,5 @@
 // 명명규칙은 xxxxIcon으로 통일
+export { default as BackIcon } from './btn_back.svg';
+export { default as BagIcon } from './btn_bag.svg';
+export { default as SearchIcon } from './btn_search.svg';
 export { default as LogoIcon } from './logo.svg';
--- src/pages/PremiumPage.tsx
+++ src/pages/PremiumPage.tsx
@@ -2,17 +2,29 @@ import styled from 'styled-components';
 
 const PremiumPage = () => {
   return (
-    <div>
+    <St.HeaderWrapper>
       <St.Header> 프리미엄관 </St.Header>
-    </div>
+    </St.HeaderWrapper>
   );
 };
 
 export default PremiumPage;
 
 const St = {
-  Header: styled.h1`
-    font-size: 10rem;
+  HeaderWrapper: styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width : 375px;
+    height : 46px;
+  
+    background: #FFFFFF;
+    box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.05);
+  `,
+
+  Header: styled.header`
     color: black;
   `,
+
 };
