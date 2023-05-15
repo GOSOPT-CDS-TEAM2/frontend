e855782c35abbccca83592797740ffea3360bbba
HeeJungPark <phj7214na@naver.com>
2023-05-15 11:17:24 +0900

    PremiumPage setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable comma-dangle */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
---
+++ src/pages/PremiumPage.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const PremiumPage = () => {
+  return (
+    <div>
+      <St.Header> 프리미엄관 </St.Header>
+    </div>
+  );
+};
+
+export default PremiumPage;
+
+const St = {
+  Header: styled.h1`
+    font-size: 10rem;
+    color: black;
+  `,
+};
