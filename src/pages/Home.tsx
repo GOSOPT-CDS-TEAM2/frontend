5628b14d9a47cd6e9db7f3073eda062f172cdb58
seobbang <seobbangway@naver.com>
2023-05-18 02:42:34 +0900

    docs: 필요 없는 파일 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
 import { LogoIcon } from '../assets/icon';
-import Product from '../components/common/Product';
 
 const Home = () => {
   return (
     <div>
       <St.Header>Vite + React</St.Header>
       <img src={LogoIcon} alt="로고" />
-      <Product />
     </div>
   );
 };
