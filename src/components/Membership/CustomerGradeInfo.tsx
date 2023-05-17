a19a7fed4045b559a7c4dc37d0c35fe8ee2ea8a3
simeunseo <rkdcjfehowl@naver.com>
2023-05-18 01:30:20 +0900

    feat/#4: 멤버십 페이지 등급별 혜택 보기 버튼 구현

--- src/components/Membership/CustomerGradeInfo.tsx
+++ src/components/Membership/CustomerGradeInfo.tsx
@@ -77,29 +77,34 @@ const CustomerGradeInfo = () => {
 
   return (
     <St.CustomerGradeInfoContainer>
-      <section className="customerGradeInfo">
-        <div className="description">
-          <h1>
-            {customerInfo.name}님의 등급
-            <br /> {grade} OLIVE
-          </h1>
+      <div>
+        <section className="customerGradeInfo">
+          <div className="description">
+            <h1>
+              {customerInfo.name}님의 등급
+              <br /> {grade} OLIVE
+            </h1>
+            <p>
+              현재 등급 유지까지 <span>{customerInfo.remainDate}</span> 일 남았어요!
+            </p>
+          </div>
+          <span className="point">P</span>
+        </section>
+        <aside className="speechBubble">
+          <img src={SpeechBubbleImg} alt="회원 등급까지 남은 금액을 설명하는 말풍선" />
           <p>
-            현재 등급 유지까지 <span>{customerInfo.remainDate}</span> 일 남았어요!
+            <span className="remainAmount">{remainAmount.toLocaleString()}원 </span>추가 구매 시<br />
+            {customerInfo.upgradePeriod} <span className="nextGrade">{nextGrade} OLIVE</span>
           </p>
-        </div>
-        <span className="point">P</span>
-      </section>
-      <aside className="speechBubble">
-        <img src={SpeechBubbleImg} alt="회원 등급까지 남은 금액을 설명하는 말풍선" />
-        <p>
-          <span className="remainCostCnt">{remainAmount.toLocaleString()}원 </span>추가 구매 시<br />
-          {customerInfo.upgradePeriod} <span className="upgradeCustomerGrade">{nextGrade} OLIVE</span>
-        </p>
-      </aside>
-      <figure className="gradeBar">
-        <img src={GradeBarImg} alt="등급 위치를 알려주는 바"></img>
-        <ol>{gradeList}</ol>
-      </figure>
+        </aside>
+        <figure className="gradeBar">
+          <img src={GradeBarImg} alt="등급 위치를 알려주는 바"></img>
+          <ol>{gradeList}</ol>
+        </figure>
+      </div>
+      <button>
+        <span className="point">P</span>등급별 혜택 보기
+      </button>
     </St.CustomerGradeInfoContainer>
   );
 };
@@ -112,90 +117,129 @@ const St = {
     flex-direction: column;
 
     width: 100%;
-    height: 26rem;
 
-    padding: 1.8rem 1.5rem 2.8rem 1.5rem;
+    & > div {
+      height: 26rem;
 
-    background: ${({ theme }) => theme.colors.pink_500};
+      padding: 1.8rem 1.5rem 2.8rem 1.5rem;
 
-    .customerGradeInfo {
-      display: flex;
-      justify-content: space-between;
+      background: ${({ theme }) => theme.colors.pink_500};
 
-      width: 100%;
-    }
-
-    .description {
-      color: ${({ theme }) => theme.colors.gray_000};
-      & > h1 {
-        ${({ theme }) => theme.fonts.SubHead1};
-      }
-
-      & > p {
-        ${({ theme }) => theme.fonts.SubTitle4};
-        & > span {
-          ${({ theme }) => theme.fonts.Head3};
-        }
-      }
-    }
-
-    .point {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      ${({ theme }) => theme.fonts.Head1}
-      color: ${({ theme }) => theme.colors.pink_100};
-      background: ${({ theme }) => theme.colors.gray_000};
-
-      width: 7rem;
-      height: 7rem;
-
-      border-radius: 50%;
-    }
-
-    .speechBubble {
-      text-align: center;
-      position: relative;
-
-      margin-top: 2.5rem;
-
-      color: ${({ theme }) => theme.colors.gray_000};
-
-      & > p {
-        position: absolute;
-        top: 1.3rem;
-
-        width: 100%;
-
-        ${({ theme }) => theme.fonts.SubTitle4}
-      }
-
-      & > p > .remainCostCnt,
-      .upgradeCustomerGrade {
-        ${({ theme }) => theme.fonts.SubTitle1}
-      }
-    }
-    .gradeBar {
-      width: 100%;
-
-      margin-top: 1.2rem;
-
-      & > ol {
+      .customerGradeInfo {
         display: flex;
         justify-content: space-between;
+
+        width: 100%;
+      }
+
+      .description {
+        color: ${({ theme }) => theme.colors.gray_000};
+        & > h1 {
+          ${({ theme }) => theme.fonts.SubHead1};
+        }
+
+        & > p {
+          ${({ theme }) => theme.fonts.SubTitle4};
+          & > span {
+            ${({ theme }) => theme.fonts.Head3};
+          }
+        }
+      }
+
+      .point {
+        display: flex;
+        justify-content: center;
         align-items: center;
 
-        margin-top: 0.6rem;
+        ${({ theme }) => theme.fonts.Head1}
+        color: ${({ theme }) => theme.colors.pink_100};
+        background: ${({ theme }) => theme.colors.gray_000};
 
-        color: ${({ theme }) => theme.colors.gray_200};
+        width: 7rem;
+        height: 7rem;
 
-        ${({ theme }) => theme.fonts.SubTitle4}
+        border-radius: 50%;
+      }
 
-        & > .selected {
-          ${({ theme }) => theme.fonts.SubTitle1}
-          color: ${({ theme }) => theme.colors.gray_000};
+      .speechBubble {
+        text-align: center;
+        position: relative;
+
+        margin-top: 2.5rem;
+
+        color: ${({ theme }) => theme.colors.gray_000};
+
+        & > p {
+          position: absolute;
+          top: 1.3rem;
+
+          width: 100%;
+
+          ${({ theme }) => theme.fonts.SubTitle4}
         }
+
+        & > p > .remainAmount,
+        .nextGrade {
+          ${({ theme }) => theme.fonts.SubTitle1}
+        }
+      }
+      .gradeBar {
+        width: 100%;
+
+        margin-top: 1.2rem;
+
+        & > ol {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          margin-top: 0.6rem;
+
+          color: ${({ theme }) => theme.colors.gray_200};
+
+          ${({ theme }) => theme.fonts.SubTitle4}
+
+          & > .selected {
+            ${({ theme }) => theme.fonts.SubTitle1}
+            color: ${({ theme }) => theme.colors.gray_000};
+          }
+        }
+      }
+    }
+
+    & > button {
+      display: flex;
+      justify-content: center;
+
+      margin: 1.6rem 1.5rem 0 1.5rem;
+      padding: 1.5rem 0;
+
+      ${({ theme }) => theme.fonts.SubTitle1}
+      color:${({ theme }) => theme.colors.gray_700};
+
+      background: ${({ theme }) => theme.colors.gray_000};
+
+      border: 0.1rem solid ${({ theme }) => theme.colors.gray_200};
+      border-radius: 0.4rem;
+
+      cursor: pointer;
+
+      .point {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin-right: 0.8rem;
+        padding-left: 0.1rem;
+
+        width: 1.7rem;
+        height: 1.7rem;
+
+        ${({ theme }) => theme.fonts.Caption1}
+        color: ${({ theme }) => theme.colors.gray_000};
+        background: ${({ theme }) => theme.colors.pink_500};
+
+        border-radius: 50%;
       }
     }
   `,
