024b8b5b8147ea9b04f557d994fc2ac5884cdeb7
simeunseo <rkdcjfehowl@naver.com>
2023-05-17 17:51:02 +0900

    feat/#6: 카테고리 nav 구현

--- src/pages/Membership.tsx
+++ src/pages/Membership.tsx
@@ -10,6 +10,7 @@ const Membership = () => {
       <Header />
       <CategoryNav />
       <CustomerGradeInfo />
+<<<<<<< HEAD
     </St.MembershipContainer>
   );
 };
@@ -35,6 +36,8 @@ const Membership = () => {
   return (
     <St.MembershipContainer>
       <Header />
+=======
+>>>>>>> c482c7d (feat/#4: 카테고리 nav 구현)
     </St.MembershipContainer>
   );
 };
@@ -42,5 +45,16 @@ const Membership = () => {
 export default Membership;
 
 const St = {
-  MembershipContainer: styled.div``,
+  MembershipContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100vw;
+
+    overflow-x: hidden;
+    /* & > main > img {
+      width: 100%;
+    } */
+  `,
 };
