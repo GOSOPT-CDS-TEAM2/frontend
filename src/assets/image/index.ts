257d723fed0a2e79f2ab8f8164e8d1028c3201fc
simeunseo <rkdcjfehowl@naver.com>
2023-05-18 00:58:52 +0900

    feat/#6: 멤버십 페이지 현재 회원 등급 정보 구현

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,2 +1,5 @@
 // 명명규칙은 xxxxImg으로 통일
-// export { ReactComponent as AdobeLogo } from './AdobeCorporateLogo1.svg';
+export { default as AdSpringImg } from './ad_spring.svg';
+export { default as GradeBarImg } from './grade_bar.svg';
+export { default as MainThumbNailImg } from './main_thumbnail.svg';
+export { default as SpeechBubbleImg } from './speech_bubble.svg';
