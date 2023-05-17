87074e5cb431e96fec62258509bd50b3739de3b4
simeunseo <rkdcjfehowl@naver.com>
2023-05-18 00:29:18 +0900

    "design/#4: nav font, margin, height 변경"

--- src/components/Membership/CategoryNav.tsx
+++ src/components/Membership/CategoryNav.tsx
@@ -28,23 +28,23 @@ const St = {
     display: flex;
     justify-content: center;
 
-    height: 3.6rem;
     padding: 0;
+    margin-top: 1.1rem;
 
     background: none;
     border: none;
   `,
   Category: styled.p`
     height: 2.6rem;
-    margin-top: 0.7rem;
+    margin: 0;
 
-    ${({ theme }) => theme.fonts.Title2};
+    height: 3.3rem;
+
+    ${({ theme }) => theme.fonts.SubHead3};
     color: ${({ theme }) => theme.colors.gray_500};
 
     &.selected {
-      margin-top: 0.9rem;
-
-      ${({ theme }) => theme.fonts.Title1};
+      ${({ theme }) => theme.fonts.SubHead2};
 
       color: ${({ theme }) => theme.colors.gray_900};
       border-bottom: 0.2rem solid ${({ theme }) => theme.colors.gray_900};
