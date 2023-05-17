f4ad3150816c609d2b5c1db0e99837894d256456
simeunseo <rkdcjfehowl@naver.com>
2023-05-18 05:36:01 +0900

    fix/#4: merge 오류 수정

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,8 +1,6 @@
 // 명명규칙은 xxxxIcon으로 통일
 export { default as AddressIcon } from './btn_address.svg';
 export { default as BackIcon } from './btn_back.svg';
-export { default as BackIcon } from './btn_back.svg';
-export { default as BagIcon } from './btn_bag.svg';
 export { default as BagIcon } from './btn_bag.svg';
 export { default as CloseIcon } from './btn_close.svg';
 export { default as LogoIcon } from './btn_logo.svg';
