bf8463c8d6e839b58d47eebc705eb231e8ad0dee
simeunseo <rkdcjfehowl@naver.com>
2023-05-19 04:14:10 +0900

    refactor/#15: EVENT_LIST data폴더로 옮기기

--- src/components/Membership/EventSection.tsx
+++ src/components/Membership/EventSection.tsx
@@ -12,9 +12,9 @@ interface Event {
 }
 
 const EVENT_LIST: Event[] = [
-  { imgUrl: MemberShipEvent1, link: '/#', description: '첫 구매 100원부터' },
-  { imgUrl: MemberShipEvent2, link: '/#', description: '슈퍼브랜드위크 선착순 특가 990원' },
-  { imgUrl: MemberShipEvent3, link: '/#', description: '5월에도 올리브영을 선물해' },
+  { imgUrl: MemberShipEvent1, link: '#', description: '첫 구매 100원부터' },
+  { imgUrl: MemberShipEvent2, link: '#', description: '슈퍼브랜드위크 선착순 특가 990원' },
+  { imgUrl: MemberShipEvent3, link: '#', description: '5월에도 올리브영을 선물해' },
 ];
 
 const EventSection = () => {
