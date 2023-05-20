20aadb107c8a89cca68eb0da4c8da9f346f19a88
seobbang <seobbangway@naver.com>
2023-05-21 02:22:28 +0900

    feat/#20: 가로 스크롤 좌우 여백영역 재조정

--- src/components/Main/HealthyLife.tsx
+++ src/components/Main/HealthyLife.tsx
@@ -7,8 +7,10 @@ const HealthyLife = () => {
     <St.HealthyLifeContainer>
       <St.Title>Healthy Life</St.Title>
       <St.ProductContainer>
+        <div />
         <img src={심드렁쿠키바Img} alt="깨가 솟아지는 방앗간" />
         <img src={깨가솟아지는방앗간Img} alt="깨가 솟아지는 방앗간" />
+        <div />
       </St.ProductContainer>
     </St.HealthyLifeContainer>
   );
@@ -19,9 +21,10 @@ export default HealthyLife;
 const St = {
   HealthyLifeContainer: styled.section`
     margin-top: 1.3rem;
-    margin-left: 1.4rem;
   `,
   Title: styled.h1`
+    margin-left: 1.4rem;
+
     ${({ theme }) => theme.fonts.Head4};
     color: ${({ theme }) => theme.colors.gray_900};
   `,
@@ -31,13 +34,17 @@ const St = {
 
     width: 100vw;
 
-    padding: 0 1.4rem;
     margin-top: 1.6rem;
 
     overflow: scroll;
 
     &::-webkit-scrollbar {
       display: none;
+    }
+
+    & > div {
+      width: 1.4rem;
+      height: 2.6rem;
     }
   `,
 };
