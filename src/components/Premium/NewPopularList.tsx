9abc07379f779d678b2ef81992739cb113eb81fa
HeeJungPark <phj7214na@naver.com>
2023-05-22 18:20:53 +0900

    feat/#18:newpupularProduct 생성

--- src/components/Premium/NewPopularList.tsx
+++ src/components/Premium/NewPopularList.tsx
@@ -1,11 +1,120 @@
 import styled from 'styled-components';
 
-const NewPopularList = () => {
- 
-  return (
-    <h1> NewPopularList </h1>
+import { 더바디샵티트리오일, 록시땅로즈오드뚜왈렛, 비오템옴드모이스춰라이저, 비오템옴드젤로션, 오리진스앰플크림, 헤라파우더 } from '../../assets/image';
+import { ProductData } from '../../types/common';
 
+import NewPopularProduct from './NewPopularProduct';
+
+const NewPopularList = () => {
+
+  const ProductData: ProductData[] = [
+    {
+      name : '오리진스 MAD 플러스 리주버네이팅 수분 앰플 크림50ml',
+      originalPrice: 54000,
+      discountRate: 20,
+      discountPrice: 43200,
+      image: 오리진스앰플크림,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: true,
+        증정: true,
+        '1+1': false,
+      },
+    },
+    {
+      name : '록시땅 로즈 오 드 뚜왈렛 75ml (2022)',
+      originalPrice: 70000,
+      discountRate: 10,
+      discountPrice: 71100,
+      image: 록시땅로즈오드뚜왈렛,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: false,
+        증정: false,
+        '1+1': false,
+      },
+    },
+    {
+      name : '헤라 소프트 피니시 루스 파우더 15g',
+      originalPrice: 60000,
+      discountRate: 0,
+      discountPrice: 60000,
+      image: 헤라파우더,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: true,
+        증정: false,
+        '1+1': false,
+      },
+    },
+    {
+      name : '더비다샵 티트리 오일 10ml',
+      originalPrice:14000,
+      discountRate: 5,
+      discountPrice: 13300,
+      image: 더바디샵티트리오일,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: true,
+        증정: false,
+        '1+1': false,
+      },
+    },
+    {
+      name : '[박보검 PICK] 비오템옴드 포스수프림 모이스춰라이저 50ml',
+      originalPrice: 140000,
+      discountRate: 0,
+      discountPrice: 140000,
+      image: 비오템옴드모이스춰라이저,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: true,
+        증정: false,
+        '1+1': false,
+      },
+    },
+    {
+      name : '비오템옴드 포스수프림 어드밴스드 젤 로션 75ml',
+      originalPrice: 90000,
+      discountRate: 0,
+      discountPrice: 90000,
+      image: 비오템옴드젤로션,
+      likeTF: false,
+      tags: {
+        BEST: false,
+        단독: false,
+        오늘드림: true,
+        증정: true,
+        '1+1': false,
+      },
+    },
+  ];
+
+  const ProductList = ProductData.map((item) => {
+    return <NewPopularProduct key = {item.name} productData = {item} />;
+  });
+
+  return (
+    <St.ProductContainer>
+      {ProductList}
+    </St.ProductContainer>
   );
 };
 
 export default NewPopularList;
+
+const St = {
+  ProductContainer : styled.section`
+  display: flex;
+  `,
+};
--- src/components/Premium/NewPopularProduct.tsx
+++ src/components/Premium/NewPopularProduct.tsx
@@ -1,14 +1,106 @@
 import styled from 'styled-components';
 
-import { BagGrayIcon, HeartFillIcon, HeartIcon } from '../../assets/icon';
-import ProductTag from '../../components/common/ProductTag';
+import { BagGrayIcon, HeartIcon } from '../../assets/icon';
 import { ProductProps } from '../../types/common';
+import ProductTag from '../common/ProductTag';
 
-const NewPopularProduct = () => {
+const NewPopularProduct = (props: ProductProps) => {
+
+  const {
+    productData: { name, originalPrice, discountRate, discountPrice, image, likeTF, tags },
+  } = props;
+
+  const tagList = Object.entries(tags)
+    .filter(([key, value]) => value) // value가 true 인 것 filtering
+    .map(([key]) => <ProductTag key={key}>{key}</ProductTag>);
 
   return (
-    <h1> NewPopularProduct </h1>
+    <St.ProductContainer>
+      
+      <St.Image src = {image} alt = {name} />
+      <St.Name> {name} </St.Name>
+      
+      <St.OriginalPrice> {originalPrice}원 </St.OriginalPrice>
+      <St.DiscountContainer>      
+        <St.DiscountRate> {discountRate}% </St.DiscountRate>
+        <St.DiscountPrice> {discountPrice}원 </St.DiscountPrice>
+      </St.DiscountContainer>
+ 
+      <St.Tags> {tagList} </St.Tags>
+
+      <St.IconContainer>
+        <St.Icon src={HeartIcon} alt="하트 아이콘" />
+        <St.Icon src={BagGrayIcon} alt="장바구니 아이콘" />
+      </St.IconContainer>
+      
+    </St.ProductContainer>
   );
 };
 
 export default NewPopularProduct;
+
+const St = {
+  ProductContainer : styled.section`
+  display: flex;
+  flex-direction: column;
+
+  width: 16.8rem;
+  height: 32rem;  
+  `,
+
+  Image: styled.img`
+  width : 16.8rem;
+  height: 16.7rem;
+  
+  border-radius: 0.4rem;
+  `,
+
+  Name : styled.h1`
+  ${({ theme }) => theme.fonts.SubTitle3};
+  color: ${({ theme }) => theme.colors.gray_900};
+
+  width: 16.5rem;
+  margin-top: 1.1rem;
+  `,
+
+  OriginalPrice: styled.h2`
+  width: 4.5rem;
+  height: 1.4rem;
+  margin-top: 0.5rem;
+
+  text-decoration: line-through;
+  ${({ theme }) => theme.fonts.Body2};
+  color: ${({ theme }) => theme.colors.gray_300};
+  `,
+
+  DiscountContainer: styled.div`
+  margin-top:0.3rem;
+  `,
+
+  DiscountRate : styled.span`
+  margin-right: 0.5rem;
+  ${({ theme }) => theme.fonts.SubTitle1};
+  color: ${({ theme }) => theme.colors.red_300};
+  `,
+
+  DiscountPrice : styled.span`
+  ${({ theme }) => theme.fonts.SubTitle1};
+  color: ${({ theme }) => theme.colors.gray_900};
+  `,
+
+  Tags: styled.div`
+  display: flex;
+  height: 1.6rem;
+  margin-top: 0.3rem;
+  `,
+  
+  IconContainer: styled.div`
+  display: flex;
+  margin-top: 1.1rem;
+  `,
+
+  Icon: styled.img`
+  width: 3.5rem;
+  height: 3.6rem;
+  `,
+};
