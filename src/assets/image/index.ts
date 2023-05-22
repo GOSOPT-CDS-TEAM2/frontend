6d53809eb630489d06ab2113202680e76af55d62
HeeJungPark <phj7214na@naver.com>
2023-05-22 21:22:14 +0900

    fix/#11 conflict 해결

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -5,6 +5,7 @@ export { default as MainThumbNailImg } from './main_thumbnail.svg';
 export { default as 꼬달리 } from './꼬달리.svg';
 export { default as 다비네스 } from './다비네스.svg';
 export { default as 달팜 } from './달팜.svg';
+export { default as 더바디샵 } from './더바디샵.svg';
 export { default as 바비브라운 } from './바비브라운.svg';
 export { default as 바비브라운영상 } from './바비브라운영상.mp4';
 export { default as 숨 } from './숨.svg';
