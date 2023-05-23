baf112218f2f3ce92f2de0936d5842a6ed8acc27
simeunseo <rkdcjfehowl@naver.com>
2023-05-23 21:15:33 +0900

    feat/#26: cart 올리브영 배송 체크박스 체크/해제 구현

--- src/components/cart/DeliveryCheck.tsx
+++ src/components/cart/DeliveryCheck.tsx
@@ -1,35 +1,39 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 
-import { CheckBigIcon } from '../../assets/icon';
+import { CheckBigCheckedIcon, CheckBigIcon, CheckBigNotCheckedIcon, ShoppingCartIcon } from '../../assets/icon';
 
 const DeliveryCheck = () => {
   const [deliveryCheck, setDeliveryCheck] = useState(true);
   const [progressAmount, setProgressAmount] = useState('100%');
   return (
     <St.DeliveryCheckContainer>
-      <St.OliveyoungDelivery>
-        <label>
-          <input
-            type="checkbox"
-            checked={deliveryCheck}
-            onChange={() => {
-              setDeliveryCheck(!deliveryCheck);
-              setProgressAmount(progressAmount === '0%' ? '100%' : '0%');
-            }}
-          />
-          올리브영 배송
-        </label>
-        <div>
+      <St.OliveyoungDelivery deliveryCheck={deliveryCheck}>
+        <div className="delivery">
+          <label>
+            <input
+              type="checkbox"
+              checked={deliveryCheck}
+              onChange={() => {
+                setDeliveryCheck(!deliveryCheck);
+                setProgressAmount(progressAmount === '0%' ? '100%' : '0%');
+              }}
+            />
+            <h3>올리브영 배송</h3>
+          </label>
+          <span>무료 배송</span>
+        </div>
+        <St.Buttons>
           <button type="button">오늘드림 이동</button>
           <button type="button">선택삭제</button>
-        </div>
+        </St.Buttons>
       </St.OliveyoungDelivery>
+
       <St.ProgressBar progressAmount={progressAmount}>
-        <span>무료 배송</span>
         <div className="progressBarContainer">
           <progress max="100"></progress>
         </div>
+        <img src={ShoppingCartIcon} alt="쇼핑 카트 아이콘"></img>
       </St.ProgressBar>
     </St.DeliveryCheckContainer>
   );
@@ -41,14 +45,71 @@ const St = {
   DeliveryCheckContainer: styled.div`
     display: flex;
     flex-direction: column;
+
+    color: ${({ theme }) => theme.colors.gray_900};
+    box-shadow: 0 0.1rem 0 rgba(0, 0, 0, 0.05);
   `,
-  OliveyoungDelivery: styled.div`
+  OliveyoungDelivery: styled.div<{ deliveryCheck: boolean }>`
     display: flex;
     justify-content: space-between;
+
+    margin: 1.2rem 1.2rem 0 1.8rem;
+
+    ${({ theme }) => theme.fonts.Title2};
+
+    .delivery {
+      display: flex;
+      flex-direction: column;
+      gap: 0.4rem;
+
+      label {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+
+      span {
+        ${({ theme }) => theme.fonts.SubTitle2};
+        color: ${({ theme }) => theme.colors.gray_700};
+      }
+    }
+
+    input {
+      width: 2.4rem;
+      height: 2.4rem;
+      margin: 0;
+
+      appearance: none;
+      border-right: 0;
+      background-image: url(${({ deliveryCheck }) => (deliveryCheck ? CheckBigCheckedIcon : CheckBigNotCheckedIcon)});
+    }
+  `,
+  Buttons: styled.div`
+    display: flex;
+    gap: 0.4rem;
+
+    button {
+      vertical-align: center;
+
+      height: 2.4rem;
+      padding: 0.5rem 0.7rem;
+
+      border-radius: 0.4rem;
+      border: 0.1rem solid ${({ theme }) => theme.colors.gray_200};
+      color: ${({ theme }) => theme.colors.gray_700};
+      background: none;
+      ${({ theme }) => theme.fonts.SubTitle4};
+    }
   `,
   ProgressBar: styled.aside<{
     progressAmount: string;
   }>`
+    display: flex;
+    gap: 0.8rem;
+    align-items: center;
+
+    margin: 0 1.9rem 1.3rem 1.8rem;
+
     progress {
       opacity: 0;
     }
@@ -56,6 +117,7 @@ const St = {
       position: relative;
       background: #eee;
       height: 0.6rem;
+      width: 100%;
       border-radius: 2rem;
       overflow: hidden;
     }
