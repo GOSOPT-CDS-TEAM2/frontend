595247fa8c49efa5b93571d3d172c167b2abad4c
seobbang <seobbangway@naver.com>
2023-05-23 19:40:32 +0900

    feat/#13: 위클리스페셜 스와이프 구현

--- src/components/Main/WeeklySpecial.tsx
+++ src/components/Main/WeeklySpecial.tsx
@@ -1,9 +1,41 @@
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
+import SwiperCore, { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import {
+  믿고보는구매후기Img,
+  봄맞이특가Img,
+  비원츠베스트셀러2종Img,
+  올라이브기프트카드Img,
+  올리브영데이Img,
+  트러블케어Img,
+} from '../../assets/image';
 
 const WeeklySpecial = () => {
+  const AD_DATA = [
+    { imgUrl: 봄맞이특가Img, description: '봄맞이 특가 광고' },
+    { imgUrl: 믿고보는구매후기Img, description: '믿고 보는 구매 후기 광고' },
+    { imgUrl: 트러블케어Img, description: '트러블케어 광고' },
+    { imgUrl: 비원츠베스트셀러2종Img, description: '비원츠 베스트 셀러 2종 광고' },
+    { imgUrl: 올리브영데이Img, description: '올리브영데이 광고' },
+    { imgUrl: 올라이브기프트카드Img, description: '올라이브기프트카드 광고' },
+  ];
+
+  const adList = AD_DATA.map(({ imgUrl, description }) => {
+    return (
+      <SwiperSlide key={imgUrl}>
+        <St.AdImg src={imgUrl} alt={description} />
+      </SwiperSlide>
+    );
+  });
+
   return (
     <St.WeeklySpecial>
       <St.Title>Weekly Special</St.Title>
+      <St.AdContainer>
+        <Swiper>{adList}</Swiper>
+      </St.AdContainer>
     </St.WeeklySpecial>
   );
 };
@@ -19,5 +51,35 @@ const St = {
 
     ${({ theme }) => theme.fonts.Head4};
     color: ${({ theme }) => theme.colors.gray_900};
+  `,
+  AdImg: styled.img`
+    width: 12.1rem;
+    height: 12.1rem;
+  `,
+  AdContainer: styled.div`
+    display: flex;
+    justify-content: center;
+
+    & > .swiper {
+      display: flex;
+      justify-content: center;
+
+      width: 13.7rem;
+      margin-top: 1.9rem;
+
+      & > .swiper-wrapper {
+        display: flex;
+        gap: 1.6rem;
+        width: 12.1rem;
+
+        & > .swiper-slide {
+          opacity: 0.4;
+          width: 12.1rem !important;
+        }
+        & > .swiper-slide-active {
+          opacity: 1;
+        }
+      }
+    }
   `,
 };
