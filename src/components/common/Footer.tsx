b55131e12e356d5c7f510dd43d75625b04a37708
HeeJungPark <phj7214na@naver.com>
2023-05-23 19:11:57 +0900

    init/#24: initial setting

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <St.FooterContainer>
+        
+    </St.FooterContainer>
+  );
+};
+
+export default Footer;
+
+const St = {
+  FooterContainer: styled.div`
+    `,
+};
