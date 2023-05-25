8aeb514c8679a53574ea03164a1327ae5ff71784
simeunseo <rkdcjfehowl@naver.com>
2023-05-25 18:09:05 +0900

    feat/#36: 상품 체크/체크해제에 따른 총 상품금액, 할인금액 계산

--- src/components/cart/PaymentDetail.tsx
+++ src/components/cart/PaymentDetail.tsx
@@ -1,19 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+
+import { cartDataState, overallCheckState, overallQuantityState } from '../../states/cart';
+
 const PaymentDetail = () => {
+  // 전역상태: 원본 데이터, 모든 상품의 체크 여부 정보, 모든 상품의 수량 정보
+  const cartData = useRecoilValue(cartDataState);
+  const [overallCheck, setOverallCheck] = useRecoilState(overallCheckState);
+  const [overallQuantity, setOverallQuantity] = useRecoilState(overallQuantityState);
+
+  // 지역상태: 총 상품금액, 총 할인금액, 결제 예상금액
+  const [totalOriginalPrice, setTotalOriginalPrice] = useState(0);
+  const [totalDiscountedPrice, setTotalDiscountedPrice] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
+
+  let sumOriginalPrice = 0;
+  let sumDiscountPrice = 0;
+  for (const id in overallCheck) {
+    if (overallCheck[id]) {
+      const checkedProduct = cartData.cartProducts.filter((product) => product.cartProductId === Number(id))[0];
+      sumOriginalPrice += checkedProduct.originalPrice;
+      sumDiscountPrice += checkedProduct.discountPrice;
+    }
+  }
+
+  useEffect(() => {
+    setTotalOriginalPrice(sumOriginalPrice);
+    setTotalDiscountedPrice(sumOriginalPrice - sumDiscountPrice);
+  }, [sumOriginalPrice, sumDiscountPrice]);
+
   return (
     <St.PaymentDetailContainer>
       <St.PaymentInfoList>
         <St.PaymentInfo>
           <h2>총 상품금액</h2>
-          <span>71,900원</span>
+          <span>{totalOriginalPrice.toLocaleString()}원</span>
         </St.PaymentInfo>
         <St.PaymentInfo>
           <h2>총 할인금액</h2>
-          <span>71,900원</span>
+          <span>{totalDiscountedPrice.toLocaleString()}원</span>
         </St.PaymentInfo>
         <St.PaymentInfo>
           <h2>총 배송비</h2>
-          <span>71,900원</span>
+          <span>{cartData.deliveryFee.toLocaleString()}원</span>
         </St.PaymentInfo>
       </St.PaymentInfoList>
     </St.PaymentDetailContainer>
